1c4fe5bc0a39fa0823dead4cc35329323a5e1a9e
강지훈 <kangjihoon12@naver.com>
2026-03-05 14:00:41 +0900

    directive문법 v-if

--- v01/src/App.vue
+++ v01/src/App.vue
@@ -5,6 +5,11 @@ export default {
         return {
             channel: "왕초보 홈페이지 만들기", // channel라는 변수
             subscribe: 2677,
+            channel2: "directive 문법 v-text",
+            channel3: "<b>directive 문법 v-html</b>",
+            score: true,
+            score2: 60,
+            score3: 60,
         }
     },
 }
@@ -19,6 +24,31 @@ export default {
     <h2>구독자수 : {{ subscribe }}</h2>
     <!-- 보간법 안에서 자바스크립트 문법 사용가능 -->
     <p>{{ '오늘의 행운 숫자 : ' + Math.ceil(Math.random() * 45) }}</p>
+
+    <!-- directive 문법은 반드시 태그의 속성으로 써야함 -->
+    <h1 v-text="channel2"></h1>
+    <h2 v-text="subscribe"></h2>
+    
+    <!-- v-html -->
+    <p v-html="channel3"></p>
+    <p v-text="channel3"></p>
+
+    <!-- 콧수염(보간법) 자체를 출력하기 -->
+    <h1 v-pre>{{ channel }}</h1>
+
+    <!-- v-if -->
+    <h1 v-if="score">채널명 : {{ channel }}</h1>
+    <!-- v-if2 -->
+    <h2 v-if="score2 >= 90">평점 : A</h2>
+    <h2 v-else-if="score2 >= 80">평점 : B</h2>
+    <h2 v-else-if="score2 >= 70">평점 : C</h2>
+    <h2 v-else>평점 : F</h2>
+
+    <!-- v-show -->
+    <!-- v-if와 v-show 기능은 비슷하지만 차이점은 v-show는 조건이 안맞아도 렌더링이 되어있음 -->
+    <!-- Chrome 개발자도구 Elements에서 확인 가능 -->
+    <h1 v-if="score3 != 60">이 영역은 v-if에서 처리하고 있습니다.</h1>
+    <h1 v-show="score3 != 60">이 영역은 v-show에서 처리하고 있습니다.</h1>
 </template>
 
 <style>
