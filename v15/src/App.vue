90ba18488ac71b5a2f18681a894bd3abac1ca69d
강지훈 <kangjihoon12@naver.com>
2026-03-08 15:15:14 +0900

    props

---
+++ v15/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import ChildCom from '@/components/ChildComponent.vue'
+
+export default {
+    components: {
+        ChildCom
+    },
+    data() {
+        return {
+            name: '홍길동',
+            age: 12,
+        }
+    },
+    methods: {
+        ageIncrement() {
+            this.age++;
+        },
+    },
+}
+</script>
+
+<template>
+    <h1>
+        props : <br>
+        부모 컴포넌트에서 자식 컴포넌트로의 데이터 전달하는 수단(단방향 데이터 흐름)
+        상위속성(부모)에서 업데이트가 있으면 하위속성(자식)으로 전달됨 반대는 안됨
+    </h1>
+    <br>
+    <h1>실습</h1>
+    <h1>부모 컴포넌트입니다.</h1>
+    <ChildCom :name="name" :age="age" />
+    <p>상위에선 데이터 변경하여 하위에 보낼수있음(props)</p>
+    <button @click="ageIncrement">나이 변경</button>
+</template>
---
+++ v15/src/components/ChildComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    props: [
+        "name", "age"
+    ],
+    created() {
+        console.log(this.name + " " + this.age);
+    },
+    methods: {
+        propsUpdater() {
+            this.age = 30;
+        },
+    },
+}
+</script>
+
+<template>
+    <h1>자식 컴포넌트</h1>
+    <p>부모 컴포넌트에서 넘어온 Name : {{ name }}</p>
+    <p>부모 컴포넌트에서 넘어온 age : {{ age }}</p>
+    <p>상위에서 받아온 데이터를 하위에서 변경하지 못함 아래 버튼은 에러남 props는 단방향임</p>
+    <button @click="propsUpdater">클릭</button>
+</template>
